dcca40b2bd83b722c0c5552d738e97ded9569c07
GinaBonilla <gibi829@gmail.com>
2023-07-13 23:44:31 -0500

    Agregando archivos al repositorio

---
+++ src/app/inputs/inputs-routing.module.ts
@@ -0,0 +1,16 @@
+import { LocalizedString } from '@angular/compiler';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { InputComponent } from './components/input/input.component';
+
+
+const routes: Routes = [{
+  path: '',
+  component: InputComponent
+}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class InputsRoutingModule { }
